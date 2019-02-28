87631a95e13dbdbaafd40e52f244781468b2b8d1
Amir <amirm.lavasani@gmail.com>
2019-02-28 22:32:13 +0330

    bug fix union -> intersection

--- hashcode.py
+++ hashcode.py
@@ -22,25 +22,26 @@ def read_input(input_file):
 def interested(s1, s2):
     t1 = set(s1[2])
     t2 = set(s2[2])
-    return min(len(t1-t2), len(t2-t1), len(t1.union(t2)))
+    return min(len(t1-t2), len(t2-t1), len(t1.intersection(t2)))
 
 def score(sol):
     total_score = 0
     for i in xrange(len(sol) - 1):
         total_score += interested(sol[i], sol[i+1])
+        print i, interested(sol[i], sol[i+1])
     return total_score
 
 def generate_output(sol, input_file):
     num_slides = len(sol)
     with open(input_file+'.out', 'w') as f:
-        f.write(num_slides)
+        f.write(str(num_slides))
         f.write('\n')
         for slide in sol:
             f.write(' '.join([str(s) for s in slide[0]]))
             f.write('\n')
 
 def vertical_merger(in_processed):
-    pass
+    return in_processed
 
 def optimizer(final_processed):
     opt_processed = sorted(final_processed, key=lambda s: len(s[2]))
@@ -62,11 +63,11 @@ def main(input_file):
     # preform solution
     # [([id], 'H' or 'V', [tags])]
     sol = optimizer(final_processed)
-
-    # score our solution
     
-    score(sol)
-
+    # score our solution
+    s = score(sol)
+    print "Score is:\t{}".format(s)
+    
     # generate output
     generate_output(sol, input_file)
     

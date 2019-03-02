a00546fe6a6366ee124a9c50792efa0b7f1c18f5
Hamed Zaghaghi <hamed.zaghaghi@gmail.com>
2019-03-03 01:20:43 +0330

    changes in extended round

--- hashcode.py
+++ hashcode.py
@@ -1,5 +1,6 @@
 import os
 from tqdm import tqdm
+import random
 
 def read_input(input_file):
     '''
@@ -15,7 +16,7 @@ def read_input(input_file):
         splitted = line.split(' ')
         alignment = splitted[0]
         tags = splitted[2:]
-        input_touple.append(([i], alignment, set(tags)))
+        input_touple.append(([i], alignment, set(tags), 0))
     return images_num, input_touple
 
 
@@ -27,6 +28,12 @@ def interested(s1, s2):
     t2 = s2[2]
     return min(len(t1-t2), len(t2-t1), len(t1.intersection(t2)))
 
+def interested_idx(processed, i1, i2):
+    processed_len = len(processed)
+    if 0 <= i1 < processed_len and 0 <= i2 < processed_len:
+        return interested(processed[i1], processed[i2])
+    return 0
+
 def score(sol):
     '''
         score our final solution
@@ -34,7 +41,7 @@ def score(sol):
     total_score = 0
     for i in xrange(len(sol) - 1):
         total_score += interested(sol[i], sol[i+1])
-        print i, interested(sol[i], sol[i+1])
+        #print i, interested(sol[i], sol[i+1])
     return total_score
 
 def generate_output(sol, input_file):
@@ -61,31 +68,60 @@ def vertical_merger(in_processed):
         else:
             horizontals.append(image)
     verticals = sorted(verticals, key=lambda x: len(x[2]))
-    length = len(verticals)
-    for i in range(0, length / 2):
+    for i in range(0, len(verticals) - 1, 2):
         t1 = verticals[i][2]
-        t2 = verticals[length - i - 1][2]
-        horizontals.append([verticals[i][0] + verticals[length - i - 1][0], 'H', t1.union(t2)])
+        t2 = verticals[i + 1][2]
+        horizontals.append(
+            [verticals[i][0] + verticals[i + 1][0], 'H', t1.union(t2)])
     return horizontals
+
+def random_swap(opt_processed, swaps):
+    opt_processed_len = len(opt_processed)
+    for _ in tqdm(range(swaps)):
+        i1 = random.randint(0, opt_processed_len - 1)
+        i2 = random.randint(0, opt_processed_len - 1)
+        i1, i2 = max(i1, i2), min(i1, i2)
+        if i1 - i2 < 2:
+            continue
+        cur_interested = 0
+        cur_interested += interested_idx(opt_processed, i1 - 1, i1)
+        cur_interested += interested_idx(opt_processed, i1, i1 + 1)
+        cur_interested += interested_idx(opt_processed, i2 - 1, i2)
+        cur_interested += interested_idx(opt_processed, i2, i2 + 1)
+
+        swp_interested = 0
+        swp_interested += interested_idx(opt_processed, i2 - 1, i1)
+        swp_interested += interested_idx(opt_processed, i1, i2 + 1)
+        swp_interested += interested_idx(opt_processed, i1 - 1, i2)
+        swp_interested += interested_idx(opt_processed, i2, i1 + 1)
+
+        if swp_interested > cur_interested:
+            opt_processed[i1], opt_processed[i2] = opt_processed[i2], opt_processed[i1]
+    return opt_processed
 
 def optimizer(final_processed):
     '''
         optimize the list of all horizental slides into the best sequence
     '''
-    sorted_slds = sorted(final_processed, key=lambda s: len(s[2]))
+    sorted_slds = sorted(final_processed, key=lambda s: len(s[2]), reverse=True)
     done = [0] * len(sorted_slds)
     done[0] = 1
     output = [sorted_slds[0]]
 
-    for _ in tqdm(range(1, len(sorted_slds))):
+    sorted_slds_len = len(sorted_slds)
+    for i in tqdm(xrange(1, sorted_slds_len)):
         sld = output[-1]
         tags = sld[2]
         max_intr = -1
-        max_poss = max_possible_intrested(sld)
-        for j in range(len(sorted_slds)):
+        max_lookup = 0
+
+        best_match = None
+        j = i
+        k = i
+        for _ in xrange(sorted_slds_len):
+            j = (j + 1) % sorted_slds_len
             if done[j]:
                 continue
-
             other_sld = sorted_slds[j]
             other_tags = other_sld[2]
 
@@ -93,20 +129,30 @@ def optimizer(final_processed):
             if intr > max_intr:
                 max_intr = intr
                 best_match = j
+                max_lookup = 0
+            elif intr == max_intr and len(sorted_slds[j][2]) < len(sorted_slds[best_match][2]):
+                max_intr = intr
+                best_match = j
+                max_lookup = 0
+            max_lookup += 1
 
-            if intr == max_poss:
+            if max_lookup > 1000 and max_intr >= 0:
                 break
 
-        done[j] = 1
-        output.append(sorted_slds[j])
-    
+            #if intr == max_possible_intrested(sld, other_sld):
+            #    break
+
+        done[best_match] = 1
+        output.append(sorted_slds[best_match])
+
     return output
 
 def max_possible_intrested(s1, s2):
     '''
-        calculates max possible score for one slide
+        calculates max possible score between two given slides
     '''
-    return len(s1) / 2
+    return min(len(s1), len(s2)) / 2
+
 
 def main(input_file):
     # Reads input
@@ -121,14 +167,15 @@ def main(input_file):
 
         # preform solution
         # [([id], 'H' or 'V', [tags])]
-        sol = optimizer(final_processed)
-        
+        opt_processed = optimizer(final_processed)
+
+        opt_processed = random_swap(opt_processed, 10000)
         # score our solution
-        s = score(sol)
+        s = score(opt_processed)
         print "Score is:\t{}".format(s)
-        
+
         # generate output
-        generate_output(sol, input_file)
+        generate_output(opt_processed, input_file)
 
     else:
         print "File does not exists"
